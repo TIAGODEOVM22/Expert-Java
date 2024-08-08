3907eb1bbee4dfdf4b7c8ae10f611045f7f9958d
TIAGODEOVM22 <tiagodeoliveiravillalva@gmail.com>
2024-08-08 19:29:30 -0300

    feat: add UserRepository

--- user-service-api/src/main/java/br/com/tiago/user_service_api/UserServiceApiApplication.java
+++ user-service-api/src/main/java/br/com/tiago/user_service_api/UserServiceApiApplication.java
@@ -1,13 +1,28 @@
 package br.com.tiago.user_service_api;
 
+import br.com.tiago.model.enums.ProfileEnum;
+import br.com.tiago.user_service_api.entity.User;
+import br.com.tiago.user_service_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Set;
+
+@RequiredArgsConstructor
 @SpringBootApplication
-public class UserServiceApiApplication {
+public class UserServiceApiApplication implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User(null, "Tiago", "villalva@gmail.com", "123456", Set.of(ProfileEnum.ROLE_CUSTUMER)));
+	}
 }
