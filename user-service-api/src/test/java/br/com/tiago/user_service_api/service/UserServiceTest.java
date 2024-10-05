8b8724a887208edc6e0624f587dbaea55096444f
Tiago Oliveira <tiagodeoliveiravillalva@gmail.com>
2024-10-04 21:36:54 -0300

    teste: add to findAll user service

--- user-service-api/src/test/java/br/com/tiago/user_service_api/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/tiago/user_service_api/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -70,6 +71,21 @@ class UserServiceTest {
         }
         Mockito.verify(repository, times(1)).findById(anyString());
         Mockito.verify(mapper, times(0)).fromEntity(any(User.class));
+    }
+
+    @Test
+    void WhenCallFindAllThenReturnUserResponse(){
+        when(repository.findAll()).thenReturn(List.of(new User(), new User()));
+        when(mapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
+
+        final var response = service.findAll();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(2, response.size());
+        Assertions.assertEquals(UserResponse.class, response.get(0).getClass());
+
+        verify(repository, times(1)).findAll();
+        verify(mapper, times(2)).fromEntity(any(User.class));
     }
 
 }
