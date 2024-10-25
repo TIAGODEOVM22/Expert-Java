c145d014b6a9c873eeaae560b63df1136fa6b085
Tiago Oliveira <tiagodeoliveiravillalva@gmail.com>
2024-10-25 09:38:09 -0300

    test: add integration, test to find by id user controller with not found

--- user-service-api/src/test/java/br/com/tiago/user_service_api/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/br/com/tiago/user_service_api/controller/impl/UserControllerImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -38,5 +39,16 @@ class UserControllerImplTest {
                 .andExpect(jsonPath("$.password").value(entity.getPassword()))
                 .andExpect(jsonPath("$.profiles").isArray());
             repository.deleteById(userId);
+    }
+
+    @Test
+    void testFindByIdWithNotFoundException() throws Exception {
+        mockMvc.perform(get("/api/users/{id}", "123"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value( "Object not found Id: 123 Type: UserResponse"))
+                .andExpect(jsonPath("$.error").value(HttpStatus.NOT_FOUND.getReasonPhrase()))
+                .andExpect(jsonPath("$.path").value("/api/users/123"))
+                .andExpect(jsonPath("$.status").value(HttpStatus.NOT_FOUND.value()))
+                .andExpect(jsonPath("$.timeStamp").isNotEmpty());
     }
 }
